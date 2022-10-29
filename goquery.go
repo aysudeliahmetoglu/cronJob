9ad888a2a771994694f63ee3fb4908a195c355b8
aysudeliahmetoglu <aysudeliahmetoglu@gmail.com>
2022-10-29 21:04:29 +0300

    edited

--- goquery.go
+++ goquery.go
@@ -4,20 +4,23 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+
 	"github.com/PuerkitoBio/goquery"
 )
 func main(){
-	titles,err := getTitle("https://blog.golang.org")
+	titles,err := getTitle("https://www.trendyol.com/")
 	if err != nil{
 		log.Println(err)
 	}
 	fmt.Println("Titles:")
 	fmt.Printf(titles)
+	
 
 
 }
 
-func getTitle(url string)(string,error){
+func getTitle(url string) (string,error) {
+
 	resp,err := http.Get(url)
 	//Get HTML
 	if err != nil{
@@ -25,7 +28,7 @@ func getTitle(url string)(string,error){
 	}
 
 	//convert goquery doc
-	doc,err := goquery.NewDocumentFromReader(resp.Body)
+	doc, err := goquery.NewDocumentFromReader(resp.Body)
 	if err != nil{
 		return "",err
 	}
@@ -33,7 +36,7 @@ func getTitle(url string)(string,error){
 	//create list
 	title_list := ""
 	doc.Find(".title a").Each(func(i int, s *goquery.Selection){
-		title_list += "- "  s.Text() + "\n"
+		title_list += "- " + s.Text() + "\n"
 	})
 
 	return title_list,nil
